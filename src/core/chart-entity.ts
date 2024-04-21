983902e6f04a972fc7a026e14b2fb19b1ee10df6
Anton Zolotukhin <anton.zolotukhin@idealogic.dev>
2024-04-21 08:42:22 +0200

    refactor (core): Refactor dates bar and drawChart method

--- src/core/chart-entity.ts
+++ src/core/chart-entity.ts
@@ -3,18 +3,17 @@ import { Candle } from "./candle";
 import { ChartDataConfig } from "../models/chart-data";
 
 export class CandleChart {
+    private readonly ctx: CanvasRenderingContext2D;
+    private readonly canvas: HTMLCanvasElement;
     private readonly width: number;
     private readonly height: number;
-    private readonly ctx: CanvasRenderingContext2D | null;
-    private readonly chartMargin: number;
-    private readonly infoBarWidth: number;
+    private readonly chartMargin: number = 30;
+    private readonly infoBarWidth: number = 80;
+    private readonly barGap: number = 2;
     private readonly maxDisplayableBars: number;
     private data: ConvertedBarData[];
     private displayedBarsCount: number;
-    private $el: HTMLElement;
-    private canvas: HTMLCanvasElement;
     private barWidth: number = 10;
-    private barGap: number = 2;
     private viewStart = 0;
     private isDragging: boolean = false;
     private dragStartX: number = 0;
@@ -22,40 +21,40 @@ export class CandleChart {
 
     constructor(options: ChartDataConfig) {
         this.validateOptions(options);
-
         this.data = options.data;
-
         this.width = options.width || 800;
         this.height = options.height || 400;
 
-        this.$el = options.el;
-        this.$el.innerHTML = '<canvas></canvas>';
+        // Element initialization
+        this.canvas = this.initializeCanvas(options.el);
+        this.ctx = this.canvas.getContext('2d')!;
 
-        this.chartMargin = 30;
-        this.infoBarWidth = 80;
-        this.canvas = this.$el.querySelector('canvas')!;
-        this.canvas.width = this.width;
-        this.canvas.height = this.height;
-        this.canvas.width += this.infoBarWidth;
-        this.canvas.height = this.height + this.chartMargin;
-
-        this.ctx = this.canvas.getContext('2d');
-
+        // Setup dimensions based on current data and options
         this.maxDisplayableBars = Math.floor((this.width - this.infoBarWidth) / (this.barWidth + this.barGap));
-        this.displayedBarsCount = Math.min(options.data.length, this.maxDisplayableBars);
+        this.displayedBarsCount = Math.min(this.data.length, this.maxDisplayableBars);
 
+        // Attach event listeners
         this.attachEventListeners();
 
+        // Initial draw of the chart
         this.init();
-    }
-
-    private init() {
-        this.drawChart();
     }
 
     private validateOptions(options: ChartDataConfig): void {
         if (!options.el) throw new Error('[Candle Chart]: "el" option must be provided');
         if (!options.data) throw new Error('[Candle Chart]: "data" option must be provided');
+    }
+
+    private initializeCanvas(el: HTMLElement): HTMLCanvasElement {
+        el.innerHTML = '<canvas></canvas>';
+        const canvas = el.querySelector('canvas')!;
+        canvas.width = this.width + this.infoBarWidth;
+        canvas.height = this.height + this.chartMargin;
+        return canvas;
+    }
+
+    private init() {
+        this.drawChart();
     }
 
     private attachEventListeners(): void {
@@ -86,6 +85,27 @@ export class CandleChart {
         this.isDragging = false;
     }
 
+    private handleScroll(event: WheelEvent) {
+        if (event.deltaX !== 0) return;
+
+        event.preventDefault();
+        const zoomIntensity = 1;
+
+        const newDisplayedBarsCount = this.displayedBarsCount - event.deltaY / Math.abs(event.deltaY) * zoomIntensity;
+
+        const minBarsToShow = 10;
+        const maxBarsToShow = 60;
+
+        this.displayedBarsCount = Math.max(
+            minBarsToShow,
+            Math.min(newDisplayedBarsCount, maxBarsToShow)
+        );
+
+        this.barWidth = (this.width - this.infoBarWidth) / this.displayedBarsCount - this.barGap;
+
+        this.drawChart();
+    }
+
     private calculateScaleY() {
         const prices = this.data.flatMap(bar => [bar.high, bar.low]);
         const maxPrice = Math.max(...prices);
@@ -106,46 +126,10 @@ export class CandleChart {
     private drawChart() {
         if (!this.ctx) return;
 
-        const scaleY = this.calculateScaleY();
-        const minPrice = this.calculateMinPrice();
-
         this.ctx.clearRect(0, 0, this.width + this.infoBarWidth, this.canvas.height);
 
-        for (let i = 0; i < this.displayedBarsCount; i++) {
-            const barIndex = this.viewStart + i;
-            if (barIndex < this.data.length) {
-                const bar = this.data[barIndex];
-                const x = i * (this.barWidth + this.barGap);
-                new Candle(this.ctx, this.barWidth, bar, x, scaleY, minPrice, this.height).draw();
-
-                if (barIndex % this.calculateDateDisplayInterval() === 0) {
-                    this.drawDate(bar.date, x);
-                }
-            }
-        }
-
+        this.drawDatesBar()
         this.drawInfoBar();
-    }
-
-    private handleScroll(event: WheelEvent) {
-        if (event.deltaX !== 0) return;
-
-        event.preventDefault();
-        const zoomIntensity = 1;
-
-        const newDisplayedBarsCount = this.displayedBarsCount - event.deltaY / Math.abs(event.deltaY) * zoomIntensity;
-
-        const minBarsToShow = 10;
-        const maxBarsToShow = 60;
-
-        this.displayedBarsCount = Math.max(
-            minBarsToShow,
-            Math.min(newDisplayedBarsCount, maxBarsToShow)
-        );
-
-        this.barWidth = (this.width - this.infoBarWidth) / this.displayedBarsCount - this.barGap;
-
-        this.drawChart();
     }
 
     private drawInfoBar() {
@@ -168,7 +152,25 @@ export class CandleChart {
         });
     }
 
-    private drawDate(date: Date, x: number) {
+    private drawDatesBar(): void {
+        const scaleY = this.calculateScaleY();
+        const minPrice = this.calculateMinPrice();
+
+        for (let i = 0; i < this.displayedBarsCount; i++) {
+            const barIndex = this.viewStart + i;
+            if (barIndex < this.data.length) {
+                const bar = this.data[barIndex];
+                const x = i * (this.barWidth + this.barGap);
+                new Candle(this.ctx, this.barWidth, bar, x, scaleY, minPrice, this.height).draw();
+
+                if (barIndex % this.calculateDateDisplayInterval() === 0) {
+                    this.drawDateItem(bar.date, x);
+                }
+            }
+        }
+    }
+
+    private drawDateItem(date: Date, x: number) {
         if (!this.ctx) return;
 
         const padding = 5;
